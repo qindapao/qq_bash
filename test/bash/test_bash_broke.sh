9784b037f7c5337812d9049bc921e49f28618da6
QinQing <northisland2017@gmail.com>
2026-02-03 10:15:33 +0800

    ADDED: test_bash_broke.sh add an exception.

--- test/bash/test_bash_broke.sh
+++ test/bash/test_bash_broke.sh
@@ -5,9 +5,16 @@ test_crash ()
 a=$((009+1))
 }
 
+# 只有这种情况下 case 1-0 的代码才会被执行
 test_crash
 if [[ $? -ne 0 ]] ; then
-    echo "case 1"
+    echo "case 1-0"
+fi
+
+# 这里分号的作用看起来不仅仅是开始下一个命令
+# 这个开始的下一个命令看起来也是和上一个命令组合在一起的
+test_crash ; if [[ $? -ne 0 ]] ; then
+    echo "case 1-1"
 fi
 
 if ! test_crash ; then
